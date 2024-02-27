4a6d7b38fb7c26d5be9e0f12aa06dfa1039c83cd
Valentin <20223tn029@utez.edu.mx>
2024-02-26 20:35:31 -0600

    Cree DocenteHome

---
+++ src/modules/docente/DocenteHome.jsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function DocenteHome() {
+  return (
+    <View>
+      <Text>DocenteHome</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
