3f7af48007575a9fb2a8e2267faa775981100b41
Agles <20223tn005@utez.edu.mx>
2024-03-24 11:58:04 -0600

    Historial de examanes casi termiando

--- src/modules/estudiante/EstudianteHistorialEx.jsx
+++ src/modules/estudiante/EstudianteHistorialEx.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useLocation } from 'react-router-dom';
 import { FaCheck } from "react-icons/fa";
 import { RiCloseLine } from "react-icons/ri"; // Cambiado a RiCloseLine
+import { Label, Radio } from 'flowbite-react';
 function EstudianteHistorialEx() {
   const location = useLocation();
 
@@ -12,26 +13,31 @@ function EstudianteHistorialEx() {
   return (
     <div className='flex flex-col items-center'>
       <div className='flex flex-col border border-green-700 mt-4 max-w-md bg-gray-100 shadow w-full'>
-        <div>
+        <div className='flex ml-2'>
           <h1 className='text-lg font-bold text-green-700'>{examen.title}</h1>
         </div>
-        <div>
+        <div className='flex ml-3'>
           <h1 className='text-md' style={{ color: "#0C748" }}>{examen.description}</h1>
         </div>
       </div>
       <div className='flex flex-col justify-center border border-green-700 mt-4 max-w-md shadow bg-gray-100'>
         {preguntas.map((pregunta, index) => (
           <div key={index} className='flex-col w-full'>
-            <div>
+            <div className='flex ml-2'>
               <h1 className='text-lg font-bold text-green-700'>{pregunta.name}</h1>
             </div>
             <div>
               {pregunta.respuestas.map((respuesta, idx) => (
-                <div key={idx} className='mt-1 flex flex-row items-center space-between'>
-                  <div className='flex-grow ml-2'>
-                    <h1 className='text-md'>{idx + 1}.- {respuesta.nombre}</h1>
+                <div key={idx} className='mt-1 flex flex-row items-center space-between' style={respuesta.correcta ? {borderBlockColor:"#04D400"} : {borderBlockColor:"red"}}>
+                  <div className='flex-grow ml-2 '>
+                  <fieldset className="flex max-w-md flex-col gap-4">
+                    <div className="flex items-center gap-2">
+                      <Radio disabled={true} style={respuesta.correcta? {backgroundColor:"#04D400"} : {backgroundColor:"red"}}/>
+                      <Label htmlFor="united-state">{respuesta.nombre}</Label>
+                    </div>
+                  </fieldset>
                   </div>
-                  <div className='ml-2 '> 
+                  <div className='ml-2 '>
                     {respuesta.correcta ? <FaCheck color='green' /> : <RiCloseLine color='red' />}
                   </div>
                 </div>
