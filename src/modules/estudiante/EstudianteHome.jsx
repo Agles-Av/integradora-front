7e01472103c4b47954f90f88a0cad10bdaebc029
Agles <20223tn005@utez.edu.mx>
2024-04-06 18:57:07 -0600

    rediseñoHomeEstudiante2

--- src/modules/estudiante/EstudianteHome.jsx
+++ src/modules/estudiante/EstudianteHome.jsx
@@ -12,6 +12,17 @@ import { VscHistory } from "react-icons/vsc";
 
 function EstudianteHome() {
 
+
+  const timeFormat = (date) => {
+    // Get the components of the date
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are 0-indexed
+    const day = String(date.getDate()).padStart(2, '0');
+    let hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+    return `${year}-${month}-${day} a las ${hours}:${minutes}:00`;
+  };
+
   const [colors, setColors] = useState([]);
 
   useEffect(() => {
@@ -176,7 +187,8 @@ function EstudianteHome() {
                       </div>
                     </div>
                     <p className="font-normal text-gray-700 dark:text-gray-400 mt-2 ">
-                      Realizado el: {item.examen.createdAt} A las {item.hr}
+                      Realizado el: {timeFormat(new Date(item.examen.createdAt))}
+
                     </p>
                   </Card>
                 ))}
